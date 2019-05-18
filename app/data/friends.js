331c1f1be07f982dde626b5c677c4933f5b9e77f
Kevin Stokes <kevinstokes@Kevins-MacBook-Pro.local>
2019-05-18 17:12:44 -0400

    friends get request now works

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendData = [{
+    name: "Kev",
+    photo: "https://imgur.com/QzARSz6",
+    scores: [
+        3,
+        5,
+        2,
+        2,
+        4,
+        1,
+        5,
+        4,
+        2,
+        1
+    ]
+}]
+
+module.exports = friendData;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,6 @@
 // Dependencies
 // =============================================================
-var express = require("express");
+
 var path = require("path");
 
 // Sets up the Express App
