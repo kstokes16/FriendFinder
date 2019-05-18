f1c58b979ddd0abff22f009b4c94a9142a0b6f10
Kevin Stokes <kevinstokes@Kevins-MacBook-Pro.local>
2019-05-18 16:04:31 -0400

    basic server setup working

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,25 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+
+module.exports = function(app) {
+
+// GET route that displays survey
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+// default to home page
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+};
