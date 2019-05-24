5026b89d026d121c6432c53dfe6479b98eb1c005
Kevin Stokes <kevinstokes@kevins-mbp.attlocal.net>
2019-05-23 22:22:10 -0400

    fixed modal so image appears

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,13 @@
 var friendData = [
     {
     name: "Kev",
-    photo: "https://imgur.com/QzARSz6",
-    scores: [
-        3,
-        5,
-        2,
-        2,
-        4,
-        1,
-        5,
-        4,
-        2,
-        1
-    ]
+    photo: "https://i.ibb.co/q9BpW5K/Screen-Shot-2019-05-23-at-10-16-03-PM.png",
+    scores: [3,5,2,2,4,1,5,4,2,1]
 },
 {
     name: "Tess",
-    photo: "testphoto.com",
-    scores: [
-        3,
-        5,
-        2,
-        5,
-        5,
-        4,
-        3,
-        2,
-        1,
-        5
-    ]
+    photo: "https://static.livebooks.com/a8236c0643ea46a584bb9951803ea096/i/ee5c275ed35947e68de95f3e26801c09/1/4SoifmQp45JMgBnHp7ed2/Boden-Bartholomew4539-copy.jpg",
+    scores: [3,5,2,5,5,4,3,2,1,5]
 }
 ]
 
