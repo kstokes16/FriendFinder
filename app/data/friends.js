dfd7dd8ebde159c8a536b76cf13238c3c9b5e308
Kevin Stokes <kevinstokes@kevins-mbp.attlocal.net>
2019-05-24 21:35:45 -0400

    updated starting list of friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,6 +8,46 @@ var friendData = [
     name: "Tess",
     photo: "https://static.livebooks.com/a8236c0643ea46a584bb9951803ea096/i/ee5c275ed35947e68de95f3e26801c09/1/4SoifmQp45JMgBnHp7ed2/Boden-Bartholomew4539-copy.jpg",
     scores: [3,5,2,5,5,4,3,2,1,5]
+},
+{
+    name: "Rob",
+    photo: "https://pbs.twimg.com/profile_images/1095186612437188609/zQCnsPPe.jpg",
+    scores: [1,3,1,5,3,1,2,4,2,3]
+},
+{
+    name: "Miranda",
+    photo: "https://nyppagesix.files.wordpress.com/2019/04/miranda-lambert.jpg?quality=80&strip=all&w=618&h=410&crop=1",
+    scores: [5,5,1,3,2,5,1,4,4,4]
+},
+{
+    name: "Steph",
+    photo: "https://amp.businessinsider.com/images/5941acc850466b74008b466a-750-563.jpg",
+    scores: [3,3,4,4,2,2,5,5,4,1]
+},
+{
+    name: "Amanda",
+    photo: "https://www.bjpenn.com/wp-content/uploads/Amanda-Nunes-5-696x399.png",
+    scores: [2,5,1,1,2,3,3,1,2,2]
+},
+{
+    name: "Ed",
+    photo: "https://cdnph.upi.com/svc/sv/upi/4331558606767/2019/1/9560b74738053b486ef79257461cb623/Ed-Sheeran-sets-new-album-No6-Collaborations-Project-for-July.jpg",
+    scores: [5,5,3,4,5,5,3,4,2,2]
+},
+{
+    name: "Billie",
+    photo: "https://www.thefamouspeople.com/profiles/images/billie-eilish-2.jpg",
+    scores: [2,4,3,5,3,1,3,5,2,4]
+},
+{
+    name: "Khalid",
+    photo: "http://dailyorange.com/resize/800/wp-content/uploads/2019/04/22230457/Khalid1.jpeg",
+    scores: [3,3,4,2,5,1,5,3,3,3]
+},
+{
+    name: "Ariana",
+    photo: "https://vg-images.condecdn.net/image/Wxe8DwnD7nY/crop/1440/f/shot-08_191_r4_qcrgb.jpg",
+    scores: [1,1,2,4,5,3,2,3,2,3]
 }
 ]
 
